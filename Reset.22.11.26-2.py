c20e9dd8f464ef797550eec906d2b80f5b61b878
jinhyoung An <wlsgud2075naver.com>
2023-01-16 22:56:26 +0900

    22년 11월 26일 연습

---
+++ Reset.22.11.26-2.py
@@ -0,0 +1,33 @@
+divmod(5,2) # 몫과 나머지 구하는 거
+
+print(divmod(5.0,3))
+
+print(5/3) # 오차 문제는 어려우므로 나중에 실무에서 실수를 다룰 때 오차에 대해 적절한 처리가 필요함
+
+a=5
+b=1.0
+
+print(float(a))
+
+
+print(type(b))
+
+
+ab="안녕하세요"
+print(type(ab))
+
+
+
+#문자열은 str
+
+x,y,z=20,30,50 # 자바랑 c도 비슷하게 가능함
+print(x,y,z) # 이거는 파이써만 가능함
+
+q=w=e=10 #이거는 파이썬만 가능함
+print(w)
+
+x=10
+del x # print 하면 에러가 뜸
+
+o=input("안녕하세요: ")
+print(o) # 대답한 게 나옴
